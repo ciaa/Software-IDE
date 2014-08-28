45d44f881893256fbb57b2c3e973bc5fbfd5eaf0
Martin Ribelotta <martinribelotta@gmail.com>
2014-08-28 00:43:40 -0300

    Cambia el esquma de descomprecion de eclipse y soluciona bug con el instalador

--- win32/eclipse-dl.bat
+++ win32/eclipse-dl.bat
@@ -4,7 +4,7 @@ set PATH=%CD%;%PATH%
 set LOCALFILE=eclipse-cdt.zip
 set LOCALDIR=eclipse-cdt
 set LOCALJRE=jre.tar.gz
-set FINALECLIPSE_DST=%CD%\cygwin\usr\local\eclipse
+set ECLIPSE_INSTDIR=%CD%\cygwin\usr\local\
 set ECLIPSEPKGS=ilg.gnuarmeclipse.debug.gdbjtag.openocd,ilg.gnuarmeclipse.managedbuild.cross
 set ECLIPSEREPOS= http://download.eclipse.org/releases/kepler,http://gnuarmeclipse.sourceforge.net/updates
 set ECLPISEURL=http://mirrors.ibiblio.org/pub/mirrors/eclipse/technology/epp/downloads/release/luna/R/eclipse-cpp-luna-R-win32.zip
@@ -16,23 +16,21 @@ if not exist %LOCALFILE% (
 )
 if not exist eclipse (
 	echo Uncompress eclipse
-	unzip %LOCALFILE%
+	unzip %LOCALFILE% -d %ECLIPSE_INSTDIR%
 )
 if  not exist %LOCALJRE% (
 	echo Download jre
 	wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" %JREURL% -O %LOCALJRE%
 )
-if  not exist eclipse\jre (
+if  not exist %ECLIPSE_INSTDIR%\eclipse\jre (
 	echo Uncompress jre
 	gunzip -c %LOCALJRE% > tmp.tar
 	tar -x -f tmp.tar
-	move jre1.8.0_20 eclipse\jre
+	move jre1.8.0_20 %ECLIPSE_INSTDIR%\eclipse\jre
 	del/Q tmp.tar
 )
-if not exist eclipse\plugins\ilg.gnuarmeclipse.managedbuild.cross* (
+if not exist %ECLIPSE_INSTDIR%\eclipse\plugins\ilg.gnuarmeclipse.managedbuild.cross* (
 	echo Install eclipse plugins
-	cd eclipse
-	$dp0\eclipse -clean -purgeHistory -nosplash -consolelog -application org.eclipse.equinox.p2.director -repository %ECLIPSEREPOS% -installIU %ECLIPSEPKGS%
-	cd ..
+	%ECLIPSE_INSTDIR%\eclipse\eclipse -clean -purgeHistory -nosplash -consolelog -application org.eclipse.equinox.p2.director -repository %ECLIPSEREPOS% -installIU %ECLIPSEPKGS%
 )
-if not exist %FINALECLIPSE_DST% move eclipse %FINALECLIPSE_DST%
+rem if not exist %FINALECLIPSE_DST% move eclipse %FINALECLIPSE_DST%
